3cf76c57fcd417dc22e00b98cb902c7662714375
mao <1296193245@qq.com>
2023-06-29 00:53:19 +0800

    提交-ts interface

---
+++ src/view/View5.vue
@@ -0,0 +1,30 @@
+<template>
+
+</template>
+
+<script lang="ts" setup>
+
+interface Student
+{
+  id: number,
+  name: string,
+  sex: string,
+  age: number
+}
+
+const student1: Student = {id: 10001, name: "张三", sex: "男", age: 19};
+//报错，缺少age
+const student2: Student = {id: 10001, name: "张三", sex: "男"};
+//报错，多了address
+const student3: Student = {id: 10001, name: "张三", sex: "男", age: 19, address: "中国"};
+
+
+console.log(student1);
+console.log(student2);
+console.log(student3);
+
+</script>
+
+<style scoped>
+
+</style>
