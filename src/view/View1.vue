78fbba5851bda92b6a398151e3f805c122d1f585
mao <1296193245@qq.com>
2023-06-29 00:32:09 +0800

    提交

---
+++ src/view/View1.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>{{ str }}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+const str: string = 'hello world';
+
+function hello(str: string)
+{
+  console.log(str);
+}
+
+hello(str);
+
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/view/View2.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>{{ user }}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface User
+{
+  id: number
+  name: string,
+  age: number,
+}
+
+function printUser(user: User)
+{
+  console.log("user.id=" + user.id)
+  console.log("user.name=" + user.name)
+  console.log("user.age=" + user.age)
+}
+
+const user: User = {
+  id: 10001,
+  name: '张三',
+  age: 19
+}
+
+printUser(user);
+
+</script>
+
+<style scoped>
+
+</style>
