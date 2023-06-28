685381fc7b68f361bee4c954bc163494a5ed130c
mao <1296193245@qq.com>
2023-06-29 00:58:03 +0800

    提交-ts 鸭子类型

---
+++ src/view/View6.vue
@@ -0,0 +1,26 @@
+<template>
+
+</template>
+
+<script lang="ts" setup>
+
+interface Student
+{
+  id: number,
+  name: string,
+  sex: string,
+  age?: number
+}
+
+const student1: Student = {id: 10001, name: "张三", sex: "男", age: 19};
+//并不会报错，缺少age，但是会出现undefined
+const student2: Student = {id: 10001, name: "张三", sex: "男"};
+console.log(student1);
+console.log(student2);
+console.log(student2.age)
+
+</script>
+
+<style scoped>
+
+</style>
