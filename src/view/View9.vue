d9323aa94004aeee186860a0979a8bb60287382d
mao <1296193245@qq.com>
2023-06-29 14:25:07 +0800

    提交-ts 字面量类型

---
+++ src/view/View9.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+/**
+ * 打印字符串到控制台
+ * @param str 字符串
+ * @param alignment 对齐方式，只能取值left、right和center
+ */
+function print(str: string, alignment: "left" | "right" | "center")
+{
+  console.log(str, alignment)
+}
+
+print("hello", "left")
+print("hello", "right")
+print("hello", "center")
+//以下报错：Argument of type '"131412351"' is not assignable to parameter of type '"left" | "right" | "center"'
+//print("hello","131412351")
+
+
+</script>
+
+<style scoped>
+
+</style>
