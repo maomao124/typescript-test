72e5b29d8ec239fb2f5702505d938987c1b38911
mao <1296193245@qq.com>
2023-06-29 23:26:42 +0800

    提交-ts 继承与接口

---
+++ src/view/View21.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+class C1
+{
+  study()
+  {
+    console.log("C1 study")
+  }
+}
+
+class C2 extends C1
+{
+  study()
+  {
+    super.study();
+    console.log("C2 study")
+  }
+}
+
+let c: C1 = new C2()
+c.study();
+
+c = new C1()
+c.study();
+
+
+</script>
+
+<style scoped>
+
+</style>
