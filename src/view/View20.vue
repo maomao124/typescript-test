8afc8a93cb43eb9892166d97ad9f288975a10e9d
mao <1296193245@qq.com>
2023-06-29 23:15:00 +0800

    提交-ts 继承与接口

---
+++ src/view/View20.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+interface Flyable
+{
+  fly(): void
+}
+
+class Animal
+{
+  name: string;
+
+  constructor(name: string)
+  {
+    this.name = name
+  }
+}
+
+class Bird extends Animal implements Flyable
+{
+  fly()
+  {
+    console.log(`${this.name}在飞翔`)
+  }
+}
+
+const b: Flyable & Animal = new Bird("小黄鸟")
+b.fly()
+
+</script>
+
+<style scoped>
+
+</style>
